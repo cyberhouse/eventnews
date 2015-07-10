38f88665b217fa76bb53f4f4ac88f66caeac30a8
Georg Ringer <georg.ringer@gmail.com>
2015-07-10 08:03:59 +0200

    [TASK] Update ext_emconf

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,8 +24,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2',
-			'news' => '3.0.0',
+			'typo3' => '6.2.0-6.2.99',
+			'news' => '3.2.0',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
