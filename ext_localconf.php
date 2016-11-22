5bdada5792bdc946080cddfa4d89cef8c1a87f5c
Georg Ringer <georg.ringer@gmail.com>
2016-11-22 15:20:49 +0100

    [FEATURE] Override label of admin module
    This can be enabled in the EM

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,11 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 // Add new controller/action
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['switchableControllerActions']['newItems']['News->month']
-	= 'Month view';
+    = 'Month view';
 
 
 /***********
@@ -14,18 +14,18 @@ $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['switchableControllerActions']['newIt
 
 // Hide not needed fields in FormEngine
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tceforms.php']['getSingleFieldClass']['eventnews']
-	= 'GeorgRinger\\Eventnews\\Hooks\\FormEngineHook';
+    = 'GeorgRinger\\Eventnews\\Hooks\\FormEngineHook';
 
 // Update flexforms
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['Hooks/BackendUtility.php']['updateFlexforms']['eventnews']
-	= 'GeorgRinger\\Eventnews\\Hooks\\BackendUtility->update';
+    = 'GeorgRinger\\Eventnews\\Hooks\\BackendUtility->update';
 
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['GeorgRinger\\News\\Hooks\\PageLayoutView']['extensionSummary']['eventnews']
-	= 'GeorgRinger\\Eventnews\\Hooks\\PageLayoutView->extensionSummary';
+    = 'GeorgRinger\\Eventnews\\Hooks\\PageLayoutView->extensionSummary';
 
 // Extend the query
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['Domain/Repository/AbstractDemandedRepository.php']['findDemanded']['eventnews']
-	= 'EXT:eventnews/Classes/Hooks/AbstractDemandedRepository.php:GeorgRinger\\Eventnews\\Hooks\\AbstractDemandedRepository->modify';
+    = 'EXT:eventnews/Classes/Hooks/AbstractDemandedRepository.php:GeorgRinger\\Eventnews\\Hooks\\AbstractDemandedRepository->modify';
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['formDataGroup']['tcaDatabaseRecord'][\GeorgRinger\Eventnews\Backend\FormDataProvider\EventNewsRowInitializeNew::class] = [
     'depends' => [
@@ -45,8 +45,15 @@ $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Domain/Model/News'][] = '
 $GLOBALS['TYPO3_CONF_VARS']['EXT']['news']['classes']['Controller/NewsController'][] = 'eventnews';
 
 \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\SignalSlot\\Dispatcher')->connect(
-	'GeorgRinger\\News\\Domain\\Service\\NewsImportService',
-	'postHydrate',
-	'GeorgRinger\\Eventnews\\Aspect\\NewsImportAspect',
-	'postHydrate'
+    'GeorgRinger\\News\\Domain\\Service\\NewsImportService',
+    'postHydrate',
+    'GeorgRinger\\Eventnews\\Aspect\\NewsImportAspect',
+    'postHydrate'
 );
+
+$emConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['eventnews']);
+// override language files of news
+if (is_array($emConfiguration) && (bool)$emConfiguration['overrideAdministrationModuleLabel']) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['default']['EXT:news/Resources/Private/Language/locallang_modadministration.xlf'][] = 'EXT:eventnews/Resources/Private/Language/Overrides/locallang_modadministration.xlf';
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['de']['EXT:news/Resources/Private/Language/locallang_modadministration.xlf'][] = 'EXT:eventnews/Resources/Private/Language/Overrides/de.locallang_modadministration.xlf';
+}
