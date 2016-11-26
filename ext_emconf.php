6bed1019f72cac3e12c154aa71607cdeb73e3119
Georg Ringer <georg.ringer@gmail.com>
2016-11-26 12:36:28 +0100

    [TASK] Prepare release of 1.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,10 +11,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 1,
-	'version' => '1.1.1',
+	'version' => '1.2.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.9.99',
+			'typo3' => '7.6.0-7.9.99',
 			'news' => '3.2.0',
 		),
 		'conflicts' => array(),
