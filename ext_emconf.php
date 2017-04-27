bf705f372344e460d190805c99cd5c8607f528fb
Georg Ringer <georg.ringer@gmail.com>
2017-04-27 19:13:43 +0200

    [TASK] Raise requirements #11

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.2.0',
+    'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-7.9.99',
-            'news' => '3.2.0-0.0.0',
+            'typo3' => '7.6.0-8.9.99',
+            'news' => '3.2.0-6.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
