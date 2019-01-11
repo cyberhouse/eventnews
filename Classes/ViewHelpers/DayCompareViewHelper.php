1a37db76fa58e70c00a265ee00b3e3f112a8a065
Georg Ringer <georg.ringer@gmail.com>
2019-01-11 20:45:08 +0100

    [!!!][BUGFIX] Check all variants in DayCompareVH
    The usage of the VH changed and must be now used like this
    <f:if condition="{events:dayCompare(newsItem:n,demand:demand)}"></f:if>
    
    Resolves: #37

--- Classes/ViewHelpers/DayCompareViewHelper.php
+++ Classes/ViewHelpers/DayCompareViewHelper.php
@@ -3,11 +3,11 @@
 namespace GeorgRinger\Eventnews\ViewHelpers;
 
 use GeorgRinger\Eventnews\Domain\Model\Dto\Demand;
+use GeorgRinger\Eventnews\Domain\Model\News as EventNews;
 use GeorgRinger\News\Domain\Model\News;
-use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
-use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 
-class DayCompareViewHelper extends AbstractConditionViewHelper implements CompilableInterface
+class DayCompareViewHelper extends AbstractViewHelper
 {
 
     /**
@@ -24,38 +24,34 @@ class DayCompareViewHelper extends AbstractConditionViewHelper implements Compil
      * @param array|null $arguments
      * @return bool
      */
-    protected static function evaluateCondition($arguments = null)
+    public function render()
     {
 
+        $found = false;
+
         /** @var Demand $demand */
-        $demand = $arguments['demand'];
-        /** @var News $newsItem */
-        $newsItem = $arguments['newsItem'];
+        $demand = $this->arguments['demand'];
+        /** @var EventNews $newsItem */
+        $newsItem = $this->arguments['newsItem'];
 
         $currentDay = \DateTime::createFromFormat('d-m-Y H:i:s', sprintf(
-            '%s-%s-%s 00:00:01', $demand->getDay(), $demand->getMonth(), $demand->getYear()));
+            '%s-%s-%s 00:00:00', $demand->getDay(), $demand->getMonth(), $demand->getYear()));
+        $currentDay->setTimezone($newsItem->getDatetime()->getTimezone());
+        $newsBeginDate = clone $newsItem->getDatetime();
+        $newsBeginDate->setTime(0, 0);
 
-        $found = false;
-        if ($demand->getDay() > 0) {
-            $newsBeginDate = $newsItem->getDatetime()->format('Y-m-d');
-            $day = date('Y-m-d', $currentDay->getTimestamp());
-
-            if ($newsItem->getEventEnd() == 0) {
-                if ($newsBeginDate === $day) {
-                    $found = true;
-                }
-            } else {
-                $newsEndDate = clone $newsItem->getEventEnd();
-                $newsEndDate->setTime(23, 59, 59);
-                $newsBeginDate = clone $newsItem->getDatetime();
-                $newsBeginDate->setTime(0, 0);
-                $currentDay->setTime(0, 0);
-
-                if ($newsBeginDate <= $currentDay && $newsEndDate >= $currentDay) {
-                    $found = true;
-                }
-            }
+        if ($newsItem->getEventEnd() === null) {
+            $newsEndDate = clone $newsBeginDate;
+            $newsEndDate->setTime(23, 59, 59);
+        } else {
+            $newsEndDate = clone $newsItem->getEventEnd();
+            $newsEndDate->setTime(0, 0);
         }
+
+        if ($newsBeginDate <= $currentDay && $newsEndDate >= $currentDay) {
+            $found = true;
+        }
+//        print_r([$newsItem->getTitle(), (int)$found, 'day' => $currentDay, 'begin' => $newsBeginDate, 'end' => $newsEndDate]);
 
         return $found;
     }
