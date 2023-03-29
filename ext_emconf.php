53b8cc97f11d72bf0241b16c313948e8a760bf6a
Georg Ringer <georg.ringer@gmail.com>
2023-03-29 21:18:50 +0200

    [TASK] Switch to 11-12 of typo3 core suport

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => '',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '5.0.2',
+    'version' => '6.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
-            'news' => '9.2.0-10.9.9',
+            'typo3' => '11.5.0-12.5.99',
+            'news' => '11.0.0-11.99.9',
         ],
         'conflicts' => [],
         'suggests' => [],
